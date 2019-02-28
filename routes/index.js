b2e95282495b0b33147b85a5a03a15e6f0747f95
gpinkard <gbrlpnkrd@gmail.com>
2019-02-27 20:32:29 -0800

    stuff

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('studentFacing', { title: 'Express' });
+router.get('/studentlogin', function(req, res, next) {
+  res.render('studentFacing', { title: 'Student login' });
 });
 
 module.exports = router;
