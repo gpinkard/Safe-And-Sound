54ed39743ac3b22e61c6ac4bb94fa07488ebeecc
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2019-03-01 09:52:48 -0800

    idk

--- app_db/db.js
+++ app_db/db.js
@@ -3,6 +3,7 @@ var mysql = require('mysql');
 var fs  = require("fs");
 var array = fs.readFileSync('apples.txt').toString().split('\n');
 
+
 var conn = mysql.createConnection({
 	// subject to change :)
 	host: array[0],
@@ -16,9 +17,31 @@ conn.connect(function(err) {
 		throw err;
 	} else {
 		console.log("now connected to database " + conn.database);
+
 	}
 });
 
-function exampleQuery(connection, table) {
-	connection.query("SELECT * FROM " + table);
+//Get all students that have checked in, ordered by last name. Outputs to a .csv file.
+// SELECT * from Student natural join CheckIn order by lName INTO OUTFILE '/tmp/checkIn.csv'
+// FIELDS TERMINATED BY ','
+// ENCLOSED BY '"'
+// LINES TERMINATED BY '\n';
+
+
+//BEGIN TRANSACTION;
+// Insert a new student into Student. Should only run if the phone number given is not in.
+function studentTest(connection, table) {
+  conn.query(INSERT IGNORE into Student values (email, fName, lName, phoneNum);
 }
+
+
+// Insert a new CheckIn value. This should run each time someone fills out the web form.
+// INSERT into CheckIn(phoneNum, timeOf, lat, lng).
+// COMMIT TRANSACTION;​
+
+
+
+
+// function exampleQuery(connection, table) {
+// 	connection.query("SELECT * FROM " + table);
+// }
