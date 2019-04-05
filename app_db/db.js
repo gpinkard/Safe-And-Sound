a1c8d78007b4b908dc939b521723fe62ab9d8a29
gpinkard <gbrlpnkrd@gmail.com>
2019-04-05 12:03:08 -0700

    json to csv export

--- app_db/db.js
+++ app_db/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
-
 const fs  = require('fs');
 const array = fs.readFileSync('../Safe-And-Sound/app_db/db.json');
 const arrayStr = JSON.parse(array);
+const parser = require('json2csv').Parser;
 
 const conn = mysql.createConnection({
 	// subject to change :)
@@ -16,12 +16,12 @@ conn.connect( (err) => {
 	if(err) {
 		throw err;
 	} else {
-		console.log("now connected to database " + arrayStr.database);
+		console.log('now connected to database ' + arrayStr.database);
 	}
 });
 
 exports.selectQuery = (table) => {
-	connection.query("SELECT * FROM " + table);
+	connection.query('SELECT * FROM ' + table);
 };
 
 // insert student data into the database (this is when they register)
@@ -36,4 +36,25 @@ exports.getStudentData = (err) => {
 	if(err) {
 		throw(err);
 	}
+};
+
+exports.exportToCSV = (path) => {
+	conn.query('SELECT lName, fName, phoneNum, email, lat, lng, MAX(timeOf) from Student NATURAL JOIN CheckIn GROUP BY phoneNum order by lName', (err, students, fields) => {
+		if(err) throw err;
+
+		const jsonStudents = JSON.parse(JSON.stringify(students));
+
+		console.log('jsonStudents: ');
+		console.log(jsonStudents);
+
+		const csvFields = ['lName', 'fName', 'phoneNum', 'email', 'lat', 'lng', 'time'];
+		const json2CSVParser = new parser({csvFields});
+		const csvData = json2CSVParser.parse(jsonStudents);
+
+		console.log('csv:');
+		console.log(csvData);
+
+		let date = new Date();
+		fs.writeFile(path + 'report' + date + '.csv');
+	});
 };
