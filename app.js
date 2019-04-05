d807bbd223d3f4b46d87124a7af33e4ba4305f1f
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2019-04-05 12:38:41 -0700

    crosses fingers on export

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+//const session = require('flash');
 
 
 const indexRouter = require('./routes/index');
@@ -25,14 +26,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // Flash module
-app.use(session()); // session middleware
-app.use(require('flash')());
-
-app.use(function (req, res) {
-  // flash a message
-  req.flash('info', 'hello!');
-  next();
-})
+// app.use(session()); // session middleware
+// app.use(require('flash')());
+//
+// app.use(function (req, res) {
+//   // flash a message
+//   req.flash('info', 'hello!');
+//   next();
+// })
 
 
 app.use('/studentlogin', indexRouter);
