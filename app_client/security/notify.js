24de087b4d367a649f4f4e805e2a2d289ab7fdff
= <=>
2019-04-27 14:06:31 -0700

    got nodemailer working

--- app_client/security/notify.js
+++ app_client/security/notify.js
@@ -2,49 +2,50 @@ const express = require('express');
 const mailer = require('nodemailer');
 const fs = require('fs');
 
-const app_email = 'gpinkard@pugetsound.edu';
-const sec_email = 'khoefflinger@gmail.com';
-const csvFilePath = './test_csv_file.csv';
+const app_email = 'PugetsoundSafeApp@pugetsound.edu';
+const sec_email = 'gpinkard@pugetsound.edu';
 const passwordFileName = 'password.txt';
 
 const pass = fs.readFileSync(passwordFileName, 'utf-8', (err, data) => {
 	if(err) throw err;
 });
 
+/*
+  Initialize the transporter to send emails with
+*/
 const transporter = mailer.createTransport({
-	service: 'outlook',
+	host: 'webmail.pugetsound.edu',
+	port: 587,
+	secure: false,
 	auth: {
 		user: app_email,
 		pass: pass
 	}
 });
 
-console.log('transporter:');
-console.log(transporter);
-
-const sendSecurityReport = () => {
+/*
+  Sends an email to UPS security with the .csv file specified by filepath (filepath is a relative path the the file in question)
+*/
+const sendSecurityReport = (filepath) => {
+	// get date for email subject
 	var date = new Date();
 	var now = date.toDateString();
 
 	var mailOptions = {
 		from: app_email,
 		to: sec_email,
-		subject: 'SafeAndSound Report ' + now,
-		text: 'Safe And Sound security report for ' + now + ' attached.',
+		subject: 'SafeAndSound Check In Report ' + now,
+		text: 'Safe And Sound check in report for ' + now + ' attached.\n\n-The UPS-Safe Team',
 		attachments: [
 			{
 				filename: 'checkin.csv',
-				path: csvFilePath
+				path: filepath
 			}
 		]
 	};
 
 	transporter.sendMail(mailOptions, (err, info) => {
-		console.log('info:');
-		console.log(info);
 		if(err) throw err;
 		else console.log(now + ': sending email to security services');
 	});
 };
-
-sendSecurityReport();
